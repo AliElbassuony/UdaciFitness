bfea663f4e3a2a8b712e12d21a71f2178fbd2cb0
AliElbassuony <alimohamed19499@gmail.com>
2021-10-05 03:16:51 +0200

    add main functions info

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+
+const App  = ( ) => {
+    return (
+      <View >
+        <AddEntry/>
+      </View>
+    );
+} 
+
+
+export default App;
---
+++ components/AddEntry.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View , Text } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helpers';
+
+class AddEntry extends React.Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric);
+
+        this.setState(state => {
+            const count = state[metric] + step;
+
+            return {
+                ...state,
+                [metric]: count > max ? max : count,
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric);
+
+        this.setState(state => {
+            const count = state[metric] - step;
+
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count,
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value,
+        }))
+    }
+    
+    render(){
+        return ( 
+            <View>
+                {getMetricMetaInfo('bike').getIcon()}
+            </View> 
+        );
+    }
+}
+ 
+export default AddEntry;
